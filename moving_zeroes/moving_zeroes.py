79adbe0c2a937ea447d5f396bd8ec1d84b8ac15c
Katie Rose Felten <katiefelten@gmail.com>
2020-08-11 21:40:54 -0400

    :zap: single numbers and moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,16 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
-
-    pass
+    nums = []
+    i = 0
+    while i < len(arr):
+        if arr[i] == 0:
+            nums.append(arr.pop(i))
+        else:
+            i += 1
+    
+    arr.extend(nums)
+    return arr
 
 
 if __name__ == '__main__':
